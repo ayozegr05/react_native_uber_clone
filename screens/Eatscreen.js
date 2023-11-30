1c6a5e380a473f524271b4b2d86fa73235eee673
ayozegr05 <ayozegr05@gmail.com>
2023-11-30 10:54:45 +0000

    uber-clone first steps working

---
+++ screens/Eatscreen.js
@@ -0,0 +1,14 @@
+import { React } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const EatScreen = () => {
+    return (
+        <View>
+            <Text>Here is FOOD</Text>
+        </View>
+    )
+}
+
+export default EatScreen
+
+const styles = StyleSheet.create({})
